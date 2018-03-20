46ae7a9b4397a1be12716bd2f3ae4a9b639ba191
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-03-20 20:39:59 -0300

    Test class

--- src/app/1-voter/voter.component.spec.ts
+++ src/app/1-voter/voter.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { VoterComponent } from './voter.component';
+import { COMMON_DIRECTIVES } from '@angular/common/src/directives';
 
 describe('VoterComponent', () => {
 let component: VoterComponent;
@@ -13,8 +14,29 @@ let fixture: ComponentFixture<VoterComponent>;
 
     fixture = TestBed.createComponent(VoterComponent);
     component = fixture.componentInstance;
+    // fixture.nativeElement
+    fixture.detectChanges();
   });
 
-  it('', () => {
+  it('should render total votes', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    let de = fixture.debugElement.query(By.css('.vote-count'));
+    let el: HTMLElement = de.nativeElement;
+
+    expect(el.innerText).toContain(21);
+  });
+
+  it('should hightlight the upvote button if I have upvoted', () => {
+    component.myVote = 1;
+    fixture.detectChanges();
+
+
+    let de = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+
+    expect(de.classes['highlighted']).toBeTruthy();
+
   });
 });
